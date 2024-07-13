058bad2968bdf95ef6b024af7937ef59b48d37b3
Xen-alpha <hky7548@naver.com>
2024-07-13 22:57:02 +0900

    GetGameRatings.php: Fix SQL query

--- includes/GetGameRatings.php
+++ includes/GetGameRatings.php
@@ -17,17 +17,22 @@ class GetGameRatings extends SimpleHandler {
 	public $PageID;
 	
 	public function run( $category, $count ) {
+		if ($count < 1 || $count > 100) {
+			return ["result" => "FAIL: count pararmeter is out of bound"];
+		}
 		$score_num = (string) $count;
 		$services = MediaWikiServices::getInstance();
 		// TODO: After 1.42+, replica DB must be called via $services->getConnectionProvider()->getReplicaDatabase();
 		$dbaseref = wfGetDB(DB_REPLICA);
 		
 		$query = $dbaseref->select('Vote', ['page_id' => 'vote_page_id', 'votecount' => 'COUNT(*)', 'vote_average' => 'AVG(vote_value)'],
-		'', '__METHOD__', ['GROUP BY' => 'vote_page_id', 'LIMIT' => $count ]);
+		'', '__METHOD__', ['GROUP BY' => 'vote_page_id', 'ORDER BY' => 'vote_average DESC','LIMIT' => $count ]);
 		
 		$queryresult = [];
 		for ($i = 0 ; $i < $query->numRows(); $i += 1){
-			array_push($queryresult, $query->current());
+			$row = $query->current();
+			$title = Title::newFromID((int) $row["page_id"])->getTitleValue()->getText();
+			array_push($queryresult, ["pagename" => $title, "votecount" => $row["votecount"], "score" => $row["vote_average"]]);
 			$query->next();
 		}
 		
