8d0c91dd4885f14f7986321f122053db739e7276
Xen-alpha <hky7548@naver.com>
2024-07-13 23:45:08 +0900

    GetGameRatings.php: category filtering test 2

--- includes/GetGameRatings.php
+++ includes/GetGameRatings.php
@@ -32,9 +32,9 @@ class GetGameRatings extends SimpleHandler {
 		for ($i = 0 ; $i < $query->numRows(); $i += 1){
 			$row = $query->current();
 			$title = Title::newFromID((int) $row->page_id);
-			if (in_array($category, array_keys($title->getParentCategories(), $category, true), true)){
+			if (in_array($category, array_keys($title->getParentCategories(), '', true), true)){
 				$titlestr = $title->getTitleValue()->getText();
-				array_push($queryresult, ["pagename" => $titlestr, "votecount" => $row->votecount, "score" => $row->vote_average]);
+				array_push($queryresult, ["pagename" => $titlestr, "votecount" => $row->votecount, "score" => $row->vote_average, "categoryArr" => $title->getParentCategories()]);
 			}
 			$query->next();
 		}
